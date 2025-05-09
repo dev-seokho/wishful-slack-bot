65ce49311f5e8ddc256e1ba34f07dd369aeedafd
dev-seokho <loyu78@gmail.com>
2025-05-09 23:23:39 +0900

    웹훅 설정

---
+++ actions/study_notice.py
@@ -0,0 +1,25 @@
+import os
+import requests
+import pytz
+from datetime import datetime
+
+
+TIL_WEBHOOK_URL = os.environ.get('TIL_WEBHOOK_URL')
+
+def post_message(message):
+    requests.post(TIL_WEBHOOK_URL, json={"text": message})
+
+kst = pytz.timezone("Asia/Seoul")
+date_string = datetime.now(kst).strftime("%Y-%m-%d")
+TIL_NOTICE = f"""
+📆 {date_string}
+<!channel> 오늘은 스터디가 진행되는 날입니다!
+다음과 같은 포맷으로 스레드를 만든 뒤, 스터디를 진행해주세요.
+
+EX] {date_string} 성준, 석호 `라이브코딩` 스터디 스레드
+
+오늘도 화이팅입니다. 😀
+"""
+
+if __name__ == "__main__":
+    post_message(TIL_NOTICE)
--- actions/til_notice.py
+++ actions/til_notice.py
@@ -1,28 +1,30 @@
 import os
 import requests
+import pytz
 from datetime import datetime
-
 
 TIL_WEBHOOK_URL = os.environ.get('TIL_WEBHOOK_URL')
 
 def post_message(message):
     requests.post(TIL_WEBHOOK_URL, json={"text": message})
 
-date_string = datetime.now().strftime("%Y-%m-%d")
-TIL_NOTICE = f"""<!channel> 오늘의 TIL을 공유할 시간입니다! 아래의 템플릿을 참고하여 작성해주세요.
-
-**[템플릿]**
-
+kst = pytz.timezone("Asia/Seoul")
+date_string = datetime.now(kst).strftime("%Y-%m-%d")
+TIL_NOTICE = f"""
 📆 {date_string}
-⏰ 공부 시간
-> h시간 m분
+<!channel> 오늘의 TIL을 공유할 시간입니다!
 
-📚 공부한 내용
-* 공부한 내용에 대해 간단히 정리해주세요.
-* TIL을 정리한 링크를 공유해주셔도 좋습니다
+💭 구글폼 링크
+- 공부한 내용에 대해 간단히 정리해주세요.
+- TIL을 정리한 링크를 공유해주셔도 좋습니다.
+- 성장을 위해 투자한 시간이 있다면 함께 작성해주세요.
+- <https://docs.google.com/forms/d/e/1FAIpQLSddHG66sF7okeqFqYBliYYsoR6NdID3qpeLGs5-vwHGbey65Q/viewform|구글폼 작성하러 가기>
 
-💭 회고
-* 오늘의 공부를 통해 느낀 점을 자유롭게 적어주세요.
+📋 구글시트 링크
+- 구성원들이 성장하고 있는지 함께 확인해주세요.
+- <https://docs.google.com/spreadsheets/d/12t2ApDFbx6tt2j56Sw0_Fz61SgWgyO2mjWpffP0rkSk/edit?resourcekey=&gid=854817029#gid=854817029|TIL 기록 구글시트 열기>
+
+구글폼 작성을 마쳤다면 ✅ 이모지를 달아주세요. 
 """
 
 if __name__ == "__main__":
